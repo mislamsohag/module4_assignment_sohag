8cf2811dec650cad681c4482ad0e092863342475
mislamsohag <mislamsohag@gmail.com>
2023-10-04 00:30:03 +0600

    first commit

---
+++ cart.php
@@ -0,0 +1,38 @@
+<?php
+
+class Product {
+    private $id;
+    private $name;
+    private $price;
+
+    public function __construct($id, $name, $price) {
+        $this->id = $id;
+        $this->name = $name;
+        $this->price = $price;
+    }
+
+    public function getId() {
+        return $this->id;
+    }
+
+    public function getName() {
+        return $this->name;
+    }
+
+    public function getPrice() {
+        return $this->price;
+    }
+
+    // You can add setters if needed
+
+    public function __toString() {
+        return "Product ID: " . $this->id . ", Name: " . $this->name . ", Price: $" . number_format($this->price, 2);
+    }
+}
+
+// Usage example:
+$product1 = new Product(1, "Example Product 1", 19.99);
+// $product2 = new Product(2, "Example Product 2", 29.99);
+
+echo $product1 . "\n";
+// echo $product2 . "\n";
